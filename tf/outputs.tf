4116c98f113c2a756de724c2ba0b1671d7e4d3f4
chrispsheehan <chrispsheehan@gmail.com>
2024-08-28 11:23:45 +0100

    output cloudfront_dist_id

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -2,6 +2,10 @@ output "cloudfront_url" {
   value = "https://${aws_cloudfront_distribution.this.domain_name}"
 }
 
+output "cloudfront_dist_id" {
+  value = aws_cloudfront_distribution.this.id
+}
+
 output "api_gateway_url" {
   value = aws_apigatewayv2_stage.this.invoke_url
 }
