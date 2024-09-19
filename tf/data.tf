ffd630f39c7d047bacf86969a992ff7dd7183442
chrispsheehan <chrispsheehan@gmail.com>
2024-09-19 16:26:32 +0100

    open policy up

--- tf/data.tf
+++ tf/data.tf
@@ -11,22 +11,37 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "website_files_policy" {
+# data "aws_iam_policy_document" "website_files_policy" {
 
+#   version = "2012-10-17"
+#   statement {
+#     actions   = ["s3:GetObject"]
+#     resources = ["${aws_s3_bucket.website_files.arn}/*"]
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["cloudfront.amazonaws.com"]
+#     }
+
+#     condition {
+#       test     = "StringLike"
+#       variable = "AWS:SourceArn"
+#       values   = [aws_cloudfront_distribution.this.arn]
+#     }
+#   }
+# }
+
+data "aws_iam_policy_document" "website_files_policy" {
   version = "2012-10-17"
+  
   statement {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.website_files.arn}/*"]
 
+    # Allowing access to any principal (completely open to the public)
     principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-
-    condition {
-      test     = "StringLike"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.this.arn]
+      type        = "AWS"
+      identifiers = ["*"]
     }
   }
 }
