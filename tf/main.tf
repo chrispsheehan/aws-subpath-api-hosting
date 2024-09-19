e4c1c343116bc4baa883817d7735f52d95f04e9d
chrispsheehan <chrispsheehan@gmail.com>
2024-09-19 19:31:27 +0100

    rm ordered

--- tf/main.tf
+++ tf/main.tf
@@ -93,21 +93,21 @@ resource "aws_cloudfront_distribution" "this" {
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
-  # Origin for /client1
-  origin {
-    domain_name              = aws_s3_bucket.website_files.bucket_regional_domain_name
-    origin_id                = "client1-origin"
-    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
-    origin_path              = "/client1"
-  }
+  # # Origin for /client1
+  # origin {
+  #   domain_name              = aws_s3_bucket.website_files.bucket_regional_domain_name
+  #   origin_id                = "client1-origin"
+  #   origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  #   origin_path              = "/client1"
+  # }
 
-  # Origin for /client2
-  origin {
-    domain_name              = aws_s3_bucket.website_files.bucket_regional_domain_name
-    origin_id                = "client2-origin"
-    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
-    origin_path              = "/client2"
-  }
+  # # Origin for /client2
+  # origin {
+  #   domain_name              = aws_s3_bucket.website_files.bucket_regional_domain_name
+  #   origin_id                = "client2-origin"
+  #   origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  #   origin_path              = "/client2"
+  # }
 
   # Origin for the API Gateway
   origin {
@@ -169,49 +169,49 @@ resource "aws_cloudfront_distribution" "this" {
     error_caching_min_ttl = 0
   }
 
-  # Cache behavior for /client1
-  ordered_cache_behavior {
-    path_pattern           = "/client1/*"
-    target_origin_id       = "client1-origin"
-    viewer_protocol_policy = "redirect-to-https"
+  # # Cache behavior for /client1
+  # ordered_cache_behavior {
+  #   path_pattern           = "/client1/*"
+  #   target_origin_id       = "client1-origin"
+  #   viewer_protocol_policy = "redirect-to-https"
 
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
+  #   allowed_methods = ["GET", "HEAD", "OPTIONS"]
+  #   cached_methods  = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+  #   forwarded_values {
+  #     query_string = false
+  #     cookies {
+  #       forward = "none"
+  #     }
+  #   }
 
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
-    compress    = true
-  }
+  #   min_ttl     = 0
+  #   default_ttl = 3600
+  #   max_ttl     = 86400
+  #   compress    = true
+  # }
 
-  # Cache behavior for /client2
-  ordered_cache_behavior {
-    path_pattern           = "/client2/*"
-    target_origin_id       = "client2-origin"
-    viewer_protocol_policy = "redirect-to-https"
+  # # Cache behavior for /client2
+  # ordered_cache_behavior {
+  #   path_pattern           = "/client2/*"
+  #   target_origin_id       = "client2-origin"
+  #   viewer_protocol_policy = "redirect-to-https"
 
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
+  #   allowed_methods = ["GET", "HEAD", "OPTIONS"]
+  #   cached_methods  = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+  #   forwarded_values {
+  #     query_string = false
+  #     cookies {
+  #       forward = "none"
+  #     }
+  #   }
 
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
-    compress    = true
-  }
+  #   min_ttl     = 0
+  #   default_ttl = 3600
+  #   max_ttl     = 86400
+  #   compress    = true
+  # }
 
   # Ordered cache behavior for API requests
   ordered_cache_behavior {
