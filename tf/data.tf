140b35c5c0ee729db5f5bfcf28ee648b150a93ae
chrispsheehan <chrispsheehan@gmail.com>
2024-09-20 10:08:13 +0100

    revert

--- tf/data.tf
+++ tf/data.tf
@@ -11,41 +11,22 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-# data "aws_iam_policy_document" "website_files_policy" {
-
-#   version = "2012-10-17"
-#   statement {
-#     actions   = ["s3:GetObject"]
-#     resources = ["${aws_s3_bucket.website_files.arn}/*"]
-
-#     principals {
-#       type        = "Service"
-#       identifiers = ["cloudfront.amazonaws.com"]
-#     }
-
-#     condition {
-#       test     = "StringLike"
-#       variable = "AWS:SourceArn"
-#       values   = [aws_cloudfront_distribution.this.arn]
-#     }
-#   }
-# }
-
-
 data "aws_iam_policy_document" "website_files_policy" {
+
+  version = "2012-10-17"
   statement {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.website_files.arn}/*"]
 
     principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_control.oac.id}"]
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
     }
 
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "AWS:Referer"
-    #   values   = ["example.com"]
-    # }
+    condition {
+      test     = "StringLike"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.this.arn]
+    }
   }
 }
