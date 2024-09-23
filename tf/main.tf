57b96f1bf33283f42ee7549d55b5b1b127aea828
Chris Sheehan <chrispsheehan@gmail.com>
2024-09-23 15:20:18 +0100

    Sec lock down (#2)
    * use regional ep
    
    * rm oac
    
    * init auth header
    
    * custom header
    
    * Reverts

--- tf/main.tf
+++ tf/main.tf
@@ -93,14 +93,6 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 }
 
-resource "aws_cloudfront_origin_access_control" "oac" {
-  name                              = "oac-${local.domain}"
-  description                       = "OAC Policy for ${local.domain}"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
 resource "aws_cloudfront_distribution" "this" {
   enabled = true
 
@@ -111,10 +103,9 @@ resource "aws_cloudfront_distribution" "this" {
       http_port              = 80
       https_port             = 443
       origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
-
 
   # Origin for the API Gateway
   origin {
