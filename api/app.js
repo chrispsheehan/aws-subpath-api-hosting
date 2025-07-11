d4f28b49893ff2a8587f8c7ceacb461442c40af9
chrispsheehan <chrispsheehan@gmail.com>
2025-07-11 15:53:52 +0100

    debug: log out api name

--- api/app.js
+++ api/app.js
@@ -7,19 +7,22 @@ app.get('/hello', (req, res) => {
   const apiName = process.env.API_NAME || 'unknown-api';
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   res.status(200).json({
-    msg: `/hello from ${apiName}, AWS Lambda is alive!`,
+    msg: `/hello AWS Lambda is alive!`,
     ip: ip,
-    path: req.url
+    path: req.url,
+    apiName: apiName
   });
 });
 
 // Catch-all route
 app.use((req, res) => {
+  const apiName = process.env.API_NAME || 'unknown-api';
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   res.status(200).json({
     msg: `Hello from catch-all`,
     ip: ip,
-    path: req.url
+    path: req.url,
+    apiName: apiName
   });
 });
 
