974b40340c07418f101c85a711f871ebaacf29ab
chrispsheehan <chrispsheehan@gmail.com>
2025-07-23 12:26:59 +0100

    chore: output dists

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -5,3 +5,11 @@ output "domain_url" {
 output "static_bucket_name" {
   value = aws_s3_bucket.website_files.bucket
 }
+
+output "ui_dist_id" {
+  value = aws_cloudfront_distribution.ui.id
+}
+
+output "api_dist_id" {
+  value = aws_cloudfront_distribution.api_domain.id
+}
